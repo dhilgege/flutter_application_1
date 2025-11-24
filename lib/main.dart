aa36fffeb16928ffa0037d386b6ff3fcddbfbfaf
fadil <dhilgege@gmail.com>
2025-11-24 19:40:22 +0700

    tugas6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home.dart'; 
-import 'package:flutter_application_1/profilePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +10,119 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: "Flutter Demo"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  
+  List<Map<String, dynamic>> items = [];
+
+  void tambah() {
+    setState(() {
+      items.add({
+        "title": "New Item",
+        "favorite": false,
+      });
+    });
+  }
+
+
+  void deleteItem(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+  }
+
+  void toggleFavorite(int index) {
+    setState(() {
+      items[index]["favorite"] = !items[index]["favorite"];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tambah & Hapus ListView"),
+        actions: [
+          IconButton(
+            onPressed: tambah,
+            icon: const Icon(Icons.add),
+          ),
+
+          Row(
+            children: [
+              const Icon(Icons.favorite, color: Colors.red),
+              Text(
+                items.where((e) => e["favorite"] == true).length.toString(),
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(width: 16),
+            ],
+          ),
+        ],
       ),
-      home: const ShopeeHomePage(), // ← ini class yang ada di home.dart kamu
+
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: MediaQuery.of(context).size.height * 0.15,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.transparent, width: 3),
+            ),
+
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+
+                // TEXT ITEM
+                Expanded(
+                  child: Text(
+                    items[index]["title"],
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                ),
+
+                // FAVORITE BUTTON
+                IconButton(
+                  onPressed: () => toggleFavorite(index),
+                  icon: Icon(
+                    items[index]["favorite"]
+                        ? Icons.favorite
+                        : Icons.favorite_border,
+                    color: items[index]["favorite"]
+                        ? Colors.red
+                        : Colors.black,
+                  ),
+                ),
+
+                // DELETE BUTTON
+                IconButton(
+                  onPressed: () => deleteItem(index),
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
